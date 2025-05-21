0d1105e402c1a6a41ad39e5754cad048774fb37a
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-21 14:27:09 -0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
+
+const entryPoint = document.getElementById("root");
+
+ReactDOM.createRoot(entryPoint).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <>
+      {/* Logo + Navbar (potential Theme Toggler?) */}
+      <header></header>
+      <main>
+        {/* Hero-Section */}
+        <section></section>
+        {/* Skills */}
+        <section></section>
+        {/* Projects */}
+        <section></section>
+        {/* Contact */}
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
