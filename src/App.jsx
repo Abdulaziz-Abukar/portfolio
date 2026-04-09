3213c53495636a1d085e63aede1b0b07b30d07b6
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2026-04-09 13:16:25 -0600

    fixes scroll issue when moving from any location blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { AppRoutes } from "./routes/AppRoutes";
+import ScrollToTop from "./components/ScrollToTop";
 
 export const App = () => {
   return (
     <>
+      <ScrollToTop />
       <AppRoutes />
     </>
   );
